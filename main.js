50072f4942aca5f47a9344cf5447eff1692d4a3b
Gustavo-Mendess <gustavormendess@gmail.com>
2023-11-29 11:36:27 -0400

    Validação JS

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 //Arquivo Javascript - Validação de Login
+
+let btn = document.querySelector("#button");
+let email = document.querySelector("#email");
+let nome = document.querySelector("#nome");
+let senha = document.querySelector("#senha");
+let msgError = document.querySelector("#error");
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (nome.value == "" || senha.value == "" || email.value == "") {
+    msgError.textContent = "Preencha Todos os Dados!";
+    msgError.style.color = "red";
+    setTimeout(() => {
+      msgError.textContent = "";
+    }, 2000);
+    return;
+  } else {
+    nome.value = "";
+    email.value = "";
+    senha.value = "";
+    return;
+  }
+});
